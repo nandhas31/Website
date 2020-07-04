e69397c505dfc098b8772e05fd7ce9d94228a6c9
Nandha Sundaravadivel <nandhasundaravadivel@Nandhas-MBP.knology.net>
2020-07-04 14:15:37 -0400

    Added login page and mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express= require('express');
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const dbConnect = require('./db/connect');
 app.use(bodyParser.urlencoded({
     extended: true
 }))
@@ -8,23 +9,39 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs')
-app.get('/signup', (req, res) =>{
-    res.render('home');
-})
 
-app.post('/signup', (req, res) =>{
+//Renders sign up page
+app.get('/signup', (req, res) => {
+    res.render('signup');
+})
+//Check if user exists and if not adds to mongoDB users collection
+app.post('/signup', (req, res) => {
     console.log(req.body);
-    res.redirect('/login')
+    dbConnect(function (db, cb) {
+        const collection = db.collection('users');
+        collection.findOne({ username: req.body.username }).then(doc => {
+            if (doc) return false;
+            collection.insertOne({ username: req.body.username, password: req.body.password, firstName: req.body.fname, lastName: req.body.lname, email: req.body.email }, (err, result) => {
+                if (err) console.log(err);
+                cb();
+                res.redirect('login')
+                return true;
+
+            });
+        });
+    })
+    
+   
 })
-app.post('/login', (req, res) =>{
+app.post('/login', (req, res) => {
 
 })
-
-app.get('/login', (req, res) =>{
+//renders login page
+app.get('/login', (req, res) => {
     res.render('login');
 })
 
 
-app.listen(8080, () =>{
+app.listen(8080, () => {
     console.log('app running on port 8080')
 })
