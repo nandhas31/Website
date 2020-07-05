4e75a279367cdf4b4bc84183013c456b1c5bed94
Nandha Sundaravadivel <nandhasundaravadivel@Nandhas-MBP.knology.net>
2020-07-05 11:42:48 -0400

    Added passport authentication and mongoose

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const dbConnect = require('./db/connect');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+const User = require('./User.js');
+const mongoose = require('mongoose');
+const session = require("express-session");
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-
+app.use(express.static("public"));
+app.use(session({ secret: "cats" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs')
@@ -16,26 +26,38 @@ app.get('/signup', (req, res) => {
 })
 //Check if user exists and if not adds to mongoDB users collection
 app.post('/signup', (req, res) => {
+    const {username,password,fName,lName,email} = req.body;
     console.log(req.body);
-    dbConnect(function (db, cb) {
-        const collection = db.collection('users');
-        collection.findOne({ username: req.body.username }).then(doc => {
-            if (doc) return false;
-            collection.insertOne({ username: req.body.username, password: req.body.password, firstName: req.body.fname, lastName: req.body.lname, email: req.body.email }, (err, result) => {
-                if (err) console.log(err);
-                cb();
-                res.redirect('login')
-                return true;
-
-            });
-        });
+    const newUser = new User({username, password, fName, lName, email});
+    User.findOne({username:username}).then(
+      document =>{
+        if(document) {
+          console.log('failed')  
+          return res.end('failed');
+        }
+        newUser.save().then(res => console.log(res));
+        return res.end('completed')
+      }
+    ).catch(err =>{
+      console.log(err)
+      return res.end('db error');
     })
+
     
-   
-})
-app.post('/login', (req, res) => {
+
 
 })
+app.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+      if (err) { return next(err); }
+      if (!user) { return res.redirect('/login'); }
+      req.logIn(user, function(err) {
+        if (err) { return next(err); }
+        console.log('login success');
+        return res.redirect('/users/' + user.username);
+      });
+    })(req, res, next);
+  });
 //renders login page
 app.get('/login', (req, res) => {
     res.render('login');
@@ -45,3 +67,34 @@ app.get('/login', (req, res) => {
 app.listen(8080, () => {
     console.log('app running on port 8080')
 })
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+            User.findOne({ username: username }, function (err, user) {
+                if (err) { return done(err); }
+                if (!user) {
+              
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+                if (!user.password === password) {
+         
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+    
+                return done(null, user);
+            });
+    }
+));
+const db = 'mongodb://localhost:27017' //require("./config/keys").MongoURI;
+mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => console.log('MongoDB Connected'))
+ .catch(err => console.log(err)); 
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
