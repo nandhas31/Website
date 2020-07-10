95e9f039633094854d235daae500389b5fc0b3d1
Nandha Sundaravadivel <nandhasundaravadivel@MacBook-Pro.knology.net>
2020-07-10 16:29:44 -0400

    Design changes, formatting, add express-session options

--- app.js
+++ app.js
@@ -9,11 +9,9 @@ const cookieParser = require('cookie-parser');
 const User = require('./db/User');
 const mongoose = require('mongoose');
 const session = require("express-session");
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
-app.use(session({ secret: "cats" }));
+app.use(session({ secret: "cats", resave: true, saveUninitialized: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -23,59 +21,53 @@ app.set('view engine', 'ejs')
 
 //Renders sign up page
 app.get('/signup', (req, res) => {
-  res.render('signup');
-})
-//Check if user exists and if not adds to mongoDB users collection
+        res.render('signup');
+    })
+    //Check if user exists and if not adds to mongoDB users collection
 app.post('/signup', (req, res) => {
-  const { username, password, fName, lName, email } = req.body;
-  console.log(req.body);
-  const newUser = new User({ username, password, fName, lName, email });
-  User.findOne({ username: username }).then(
-    document => {
-      if (document) {
-        console.log('failed')
-        return res.end('failed');
-      }
-      newUser.save().then(res => console.log(res));
-      return res.end('completed')
-    }
-  ).catch(err => {
-    console.log(err)
-    return res.end('db error');
-  })
-})
-//logs in user
-app.post('/login', function (req, res, next) {
-  passport.authenticate('local', function (err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      console.log('made it here')
-      return res.render('login', { errMsg: 'Incorrect Password' });
-    }
-    req.logIn(user, function (err) {
-      if (err) { return next(err); }
-      console.log('login success');
-      return res.redirect('/dashboard');
-    });
-  })(req, res, next);
+        const { username, password, fName, lName, email } = req.body;
+        const newUser = new User({ username, password, fName, lName, email });
+        User.findOne({ username: username }).then(
+            document => {
+                if (document) {
+                    return res.end('failed');
+                }
+                newUser.save();
+                return res.end('completed')
+            }
+        ).catch(err => {
+            return res.end('db error');
+        })
+    })
+    //logs in user
+app.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.render('login', { errMsg: 'Incorrect Password' });
+        }
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.redirect('/dashboard');
+        });
+    })(req, res, next);
 });
 //renders login page
 app.get('/login', (req, res) => {
-  res.render('login', { errMsg: null});
+    res.render('login', { errMsg: null });
 })
 
 
 app.get('/dashboard', (req, res) => {
-  res.render('dashboard', { user: req.user })
-})
-//logs out user
-app.get('/logout', (req, res)=>{
-  req.logout();
-  res.render('login', {errMsg: 'Successfully logged out'});
+        res.render('dashboard', { user: req.user })
+    })
+    //logs out user
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.render('login', { errMsg: 'Successfully logged out' });
 })
 app.listen(8080, () => {
-  console.log('app running on port 8080')
+    console.log('app running on port 8080')
 })
-
