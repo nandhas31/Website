aad7ef1d77eb3f77c5d756f4e784d775ced7514f
Nandha Sundaravadivel <nandhasundaravadivel@MacBook-Pro.knology.net>
2020-07-26 11:10:20 -0400

    Added delete usr option

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const User = require('./db/User');
 const mongoose = require('mongoose');
 const session = require("express-session");
-const bcrypt = require ('bcryptjs');
+const bcrypt = require('bcryptjs');
 const validateEmail = require('./util/validateEmail');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
@@ -23,50 +23,50 @@ app.set('view engine', 'ejs')
 
 //Renders sign up page
 app.get('/signup', (req, res) => {
-        res.render('signup');
-    })
-    //Check if user exists and if not adds to mongoDB users collection
+    res.render('signup');
+})
+//Check if user exists and if not adds to mongoDB users collection
 app.post('/signup', (req, res) => {
-        
-        const { username, password, fName, lName, email } = req.body;
-        if (!username ||!password ||!fName ||!lName ||!email){  
-            return res.send({success:false, message:'Missing Info'});
-        }
-        if (!validateEmail(email)) return res.send({success:false, message:'Invalid Email'});
-        if (password.length < 6 || username.length < 6) return res.end('missinginfo');
-        const newUser = new User({ username, password, fName, lName, email });
-        User.findOne({ username: username }).then(
-            document => {
-                if (document) {
-                    return res.send({success:false, message:'failed'});
-                }
-                bcrypt.genSalt(10, (err, salt)=>{
-                    bcrypt.hash(newUser.password, salt,(err, hash)=>{
-                        if (err) throw err;
-                        newUser.password = hash;
-                        newUser.save();
-                        res.send({success: true, message: 'completed'});
-                    })
-                })
-                newUser.save();
-                return res.send({success:true, message:'completed'})
+
+    const { username, password, fName, lName, email } = req.body;
+    if (!username || !password || !fName || !lName || !email) {
+        return res.send({ success: false, message: 'Missing Info' });
+    }
+    if (!validateEmail(email)) return res.send({ success: false, message: 'Invalid Email' });
+    if (password.length < 6 || username.length < 6) return res.end('missinginfo');
+    const newUser = new User({ username, password, fName, lName, email });
+    User.findOne({ username: username }).then(
+        document => {
+            if (document) {
+                return res.send({ success: false, message: 'failed' });
             }
-        ).catch(err => {
-            return res.send({success:false, message:'db error'});
-        })
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save();
+                    res.send({ success: true, message: 'completed' });
+                })
+            })
+            newUser.save();
+            return res.send({ success: true, message: 'completed' })
+        }
+    ).catch(err => {
+        return res.send({ success: false, message: 'db error' });
     })
-    //logs in user
-app.post('/login', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
+})
+//logs in user
+app.post('/login', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
         if (err) {
             return next(err);
         }
         if (!user) {
-            return res.send({success:false, message: 'Invalid Credentials'});
+            return res.send({ success: false, message: 'Invalid Credentials' });
         }
-        req.logIn(user, function(err) {
+        req.logIn(user, function (err) {
             if (err) { return next(err); }
-            return res.send({success: true,message:'success'});
+            return res.send({ success: true, message: 'success' });
         });
     })(req, res, next);
 });
@@ -77,45 +77,63 @@ app.get('/login', (req, res) => {
 
 
 app.get('/dashboard', (req, res) => {
-        if (req.user)
-            res.render('dashboard', { user: req.user })
-        else {
-            res.render('login', {errMsg:null});
-        }
-    })
-    //logs out user
+    if (req.user)
+        res.render('dashboard', { user: req.user })
+    else {
+        res.render('login', { errMsg: null });
+    }
+})
+//logs out user
 app.get('/logout', (req, res) => {
     req.logout();
     res.render('login', { errMsg: 'Successfully logged out' });
 })
 
-app.get('/edit', (req, res)=>{
-    if (!req.user) return res.render('login', {errMsg:null});
+app.get('/edit', (req, res) => {
+    if (!req.user) return res.render('login', { errMsg: null });
     res.render('edit');
 })
-app.post('/changePassword', (req, res) =>{
-    if (!req.user) return res.send({success:false, message:'Forbidden'});
+app.post('/changePassword', (req, res) => {
+    if (!req.user) return res.send({ success: false, message: 'Forbidden' });
     bcrypt.compare(req.body.oldPassword, req.user.password).then(
-        result =>{
-            if (!result) res.send({sucess:false, message:'Wrong Password'});
+        result => {
+            if (!result) res.send({ sucess: false, message: 'Wrong Password' });
             else {
-                bcrypt.genSalt(10, (err, salt)=>{
-                    bcrypt.hash(req.body.newPassword, salt,(err, hash)=>{
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(req.body.newPassword, salt, (err, hash) => {
                         if (err) throw err;
-                        User.findOneAndUpdate({username:req.user.username, password:req.user.password}, {password:hash}, (err, doc)=>{
+                        User.findOneAndUpdate({ username: req.user.username, password: req.user.password }, { password: hash }, (err, doc) => {
                             if (err) {
-                                res.send({success:false, message:'unknown error'});
+                                res.send({ success: false, message: 'unknown error' });
                                 throw err;
                             }
-                            else{
-                                res.send({success:true,message:'completed'});
+                            else {
+                                res.send({ success: true, message: 'completed' });
                             }
                         })
                     })
                 })
-                
+
             }
-        }).catch(err =>{throw err})
+        }).catch(err => { throw err })
+})
+app.post('/delete', (req, user) => {
+    if (!req.user) {
+        res.send({ success: false, message: 'Not logged in' })
+    }
+    if (req.user.username === req.body.username) {
+        bcrypt.compare(req.body.password, req.user.password, (err, res) => {
+            if (err) throw err;
+            if (!res) res.send({ success: false, message: 'Incorrect Password' });
+            User.findOneAndDelete({ _id: req.user._id })
+            res.send({ success: true, message: '' })
+        })
+
+    }
+    else{
+        res.send({success:false, message:'Incorrect Username'});
+    }
+
 })
 app.listen(8080, () => {
     console.log('app running on port 8080')
